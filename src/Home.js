b79da7064cee11d0828c2a2a5b12743655fe0e6a
Kaviyathangarasu <kaviyathangarasu86@gmail.com>
2024-03-09 03:47:28 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://images.app.goo.gl/gMqM8ok9jJfc1VK68" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-ten-beryl.vercel.app"
