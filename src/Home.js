c7771808fe1ab0c842680e41d9bf8efb887faf6d
Kaviyathangarasu <kaviyathangarasu86@gmail.com>
2024-03-09 04:16:06 -0800

    update

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <h3>Welcome to React-App</h3>
-      <img className="home-img" src="https://images.app.goo.gl/gMqM8ok9jJfc1VK68" alt="Welcome picture" />
+      <img className="home-img" src="https://wallpapers.com/images/hd/high-resolution-fall-19-x-12-background-1qlfbqqs2kyn4kvg.png_" alt="Welcome picture" />
     </div>
   )
 }
